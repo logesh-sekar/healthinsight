5b11e0f350334e598c00a8d7cdc4bb8d92cd2ef1
Logesh Sekar <15341@ITCINFOTECH.com>
2018-07-18 16:00:23 +0530

    Initial version

---
+++ src/app/layout/layout-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            { path: '', redirectTo: 'dashboard', pathMatch: 'prefix' },
+            { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
+            { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
+        ]
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class LayoutRoutingModule {}
